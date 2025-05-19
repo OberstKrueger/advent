907a8e1fc2c3c0ebd341f3edd62a97820884b765
OberstKrueger <ok@oberstkrueger.com>
2025-05-19 06:26:58 -0700

    Added Default trait, adjusted imports

--- src/answer.rs
+++ src/answer.rs
@@ -1,12 +1,22 @@
-use std::fmt;
+use std::default::Default;
+use std::fmt::{Display, Formatter, Result};
 
 pub struct Answer {
     pub first: Option<i64>,
     pub second: Option<i64>
 }
 
-impl fmt::Display for Answer {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+impl Default for Answer {
+    fn default() -> Self {
+        Answer {
+            first: None,
+            second: None
+        }
+    }
+}
+
+impl Display for Answer {
+    fn fmt(&self, f: &mut Formatter<'_>) -> Result {
         match (&self.first, &self.second) {
             (Some(first), Some(second)) => write!(f, "{}, {}", first, second),
             (Some(first), None)         => write!(f, "{}, n/a", first),
