b5437909194bcf74500b18eb28d446c13e5556a5
OberstKrueger <ok@oberstkrueger.com>
2025-05-20 02:00:30 -0700

    Added 2015 day 1

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,7 @@ use std::fs::read_to_string;
 
 fn main() {
     println!("2015.01 => {}", solution_2015_01(&read_file("inputs/2015_01.txt")));
+    println!("2015.02 => {}", solution_2015_02(&read_file("inputs/2015_02.txt")));
 }
 
 fn read_file(input: &str) -> String {
--- src/solutions2015.rs
+++ src/solutions2015.rs
@@ -21,6 +21,36 @@ pub fn solution_2015_01(input: &str) -> Answer {
     answer
 }
 
+pub fn solution_2015_02(input: &str) -> Answer {
+    let mut answer = Answer {
+        first: Some(0),
+        second: None,
+    };
+
+    for line in input.lines() {
+        let mut dimensions: Vec<i64> = line.split('x')
+        .filter_map(|number| number.parse::<i64>().ok())
+        .collect();
+
+        if dimensions.len() == 3 {
+            dimensions.sort();
+
+            let side1 = dimensions[0] * dimensions[1];
+            let side2 = dimensions[0] * dimensions[2];
+            let side3 = dimensions[1] * dimensions[2];
+
+            let total = 2 * (side1 + side2 + side3);
+
+            let ribbon = 2 * dimensions[0] + 2 * dimensions[1] + dimensions.iter().product::<i64>();
+
+            answer.first = Some(answer.first.unwrap_or(0) + side1 +total);
+            answer.second = Some(answer.second.unwrap_or(0) + ribbon);
+        }
+    }
+
+    answer
+}
+
 #[cfg(test)]
 mod tests_2015 {
     use super::*;
@@ -36,8 +66,12 @@ mod tests_2015 {
         assert_eq!(solution_2015_01("))(").first, Some(-1));
         assert_eq!(solution_2015_01(")))").first, Some(-3));
         assert_eq!(solution_2015_01(")())())").first, Some(-3));
-
         assert_eq!(solution_2015_01(")").second, Some(1));
         assert_eq!(solution_2015_01("()())").second, Some(5));
+
+        assert_eq!(solution_2015_02("2x3x4").first, Some(58));
+        assert_eq!(solution_2015_02("1x1x10").first, Some(43));
+        assert_eq!(solution_2015_02("2x3x4").second, Some(34));
+        assert_eq!(solution_2015_02("1x1x10").second, Some(14));
     }
 }
